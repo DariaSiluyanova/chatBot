3696f51df2b7f4fcf9bbaa8a27f160d7226fb18c
Daria <prog-1@goodfil.com>
2023-12-06 15:08:47 +0300

    intializing project

---
+++ src/components/ChatList.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card  class="bg-purple-lighten-4 pa-2 mb-4 dialog">
+    <template v-for="message in props.messages">
+      <bot-messages v-if="message.chatbot" class="dialog-style" :key="message.id" :message="message"></bot-messages>
+      <user-messages v-else class="dialog-style" :key="message.id" :message="message"/>
+    </template>
+  </v-card>
+</template>
+
+<script setup>
+import UserMessages from './UserMessages.vue'
+import BotMessages from './BotMessages.vue';
+
+const props = defineProps(['messages'])
+</script>
+
+<style scoped>
+.dialog {
+  display: flex;
+  flex-direction: column;
+  height: 500px;
+}
+</style>
---
+++ src/layouts/default/View.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-main class="pa-8 pa-sm-12">
+    <router-view />
+  </v-main>
+</template>
+
+<script setup>
+  //
+</script>
