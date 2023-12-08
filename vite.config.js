bebf66bde95095c62fd68bd8b4c7ebb60cd246a8
Daria <dariasiluyanowa@gmail.com>
2023-12-08 19:29:16 +0300

    delete baseURL

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,6 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/chat-bot',
   plugins: [
     vue({
       template: { transformAssetUrls }
